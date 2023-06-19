9b998400a7a672c2b0c85dec7780678d2f8a993c
cyf <346515575@qq.com>
2023-06-19 10:10:44 +0800

    add 0619

---
+++ 202306/20230619/2-405数字转换为十六进制数/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+using namespace std;
+class Solution {
+public:
+	string toHex(int num) {
+
+	}
+};
