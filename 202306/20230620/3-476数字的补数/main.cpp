78881db79ff2eb1240a975f36957640d4cf7ddcc
cyf <346515575@qq.com>
2023-06-20 12:00:45 +0800

    add0620

--- 202306/20230620/3-476数字的补数/main.cpp
+++ 202306/20230620/3-476数字的补数/main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 class Solution {
 public:
     int findComplement(int num) {
-
+        vector<int> temp;
+        while (num != 0) {
+            temp.emplace_back(num & 1);
+        }
     }
 };
