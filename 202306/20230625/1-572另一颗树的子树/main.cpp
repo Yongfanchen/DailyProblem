7faa354b74b80b47194cf372d147901a18983036
cyf <346515575@qq.com>
2023-06-25 15:47:59 +0800

    add20230625

---
+++ 202306/20230625/1-572另一颗树的子树/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <cctype>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    bool isSubtree(TreeNode *root, TreeNode *subRoot)
+    {
+    }
+};
---
+++ 202306/20230625/5-594最长和谐子序列/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <cctype>
+using namespace std;
+class Solution
+{
+public:
+    int findLHS(vector<int> &nums)
+    {
+    }
+};
