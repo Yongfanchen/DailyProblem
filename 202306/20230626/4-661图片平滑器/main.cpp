6c39aa4f2e1516afafe8d7068fbf490802ab8e0c
cyf <346515575@qq.com>
2023-06-27 21:24:24 +0800

    add0626-2

--- 202306/20230626/4-661图片平滑器/main.cpp
+++ 202306/20230626/4-661图片平滑器/main.cpp
@@ -10,7 +10,64 @@ using namespace std;
 class Solution
 {
 public:
-    vector<vector<int>> imageSmoother(vector<vector<int>> &img)
-    {
-    }
+	vector<vector<int>> imageSmoother(vector<vector<int>> &img)
+	{
+		// vector<vector<int>> res;
+		int m = img.size();
+		int n = img[0].size();
+		vector<vector<int>> res(m, vector<int>(n));
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				int num = 0;
+				int sum = 0;
+				for (int x = i - 1; x <= i + 1; x++)
+				{
+					for (int y = j - 1; y <= j + 1; y++)
+					{
+						if (x >= 0 && x < m && y >= 0 && y < n)
+						{
+							num++;
+							sum += img[x][y];
+						}
+					}
+				}
+				res[i][j] = sum / num;
+			}
+
+		}
+		return res;
+	}
 };
+//±ê×¼´ð°¸
+class Solution
+{
+public:
+	vector<vector<int>> imageSmoother(vector<vector<int>>& img)
+	{
+		int m = img.size(), n = img[0].size();
+		vector<vector<int>> ret(m, vector<int>(n));
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				int num = 0, sum = 0;
+				for (int x = i - 1; x <= i + 1; x++)
+				{
+					for (int y = j - 1; y <= j + 1; y++)
+					{
+						if (x >= 0 && x < m && y >= 0 && y < n)
+						{
+							num++;
+							sum += img[x][y];
+						}
+					}
+				}
+				ret[i][j] = sum / num;
+			}
+		}
+		return ret;
+	}
+};
+
--- 202306/20230626/5-671二叉树中第二小的节点/main.cpp
+++ 202306/20230626/5-671二叉树中第二小的节点/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <set>
 #include <unordered_set>
 using namespace std;
 
@@ -22,7 +23,22 @@ struct TreeNode
 class Solution
 {
 public:
+    void inorder(TreeNode *root, set<int> &set)
+    {
+        if (root == nullptr)
+            return;
+        inorder(root->left, set);
+        set.insert(root->val);
+        inorder(root->right, set);
+    }
     int findSecondMinimumValue(TreeNode *root)
     {
+        set<int> setRes;
+        inorder(root, setRes);
+        if (setRes.size() == 1)
+            return -1;
+        auto it = setRes.begin();
+        ++it;
+        return *it;
     }
 };
