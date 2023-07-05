613b4373375aaaeb4de54426d53a4bee1ad805c7
cyf <346515575@qq.com>
2023-07-05 22:48:30 +0800

    add 0705

---
+++ 202307/20230703/6.N字形变换/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+#include <set>
+#include <array>
+using namespace std;
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+    }
+};
