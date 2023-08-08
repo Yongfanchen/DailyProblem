6fcf64bb94a7e8db04e812697b6d6b1d404a9b12
cyf <346515575@qq.com>
2023-08-08 21:58:10 +0800

    add

--- VS/LeetCode/maxProfit.cpp
+++ VS/LeetCode/maxProfit.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
 		int n = prices.size();
-		vector<vector<int>> dp(n,vector<int>(2));
+		vector<vector<int>> dp(n, vector<int>(2));
 		dp[0][0] = 0;
 		dp[0][1] = -INT_MAX;
 
@@ -24,3 +24,18 @@ public:
 		return dp[n - 1][0];
 	}
 };
+
+int maxProfit(vector<int>& prices) {
+	int n = prices.size();
+	vector<vector<int>> dp(n, vector<int>(2));
+
+	dp[0][0] = 0;
+	// Initialize the buy state with the first day's price
+	//dp[0][1] = INT_MIN;   这是不对的
+	dp[0][1] = -prices[0];
+	for (int i = 1; i < n; i++) {
+		dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+		dp[i][1] = max(dp[i - 1][1], -prices[i]);
+	}
+	return dp[n-1][0];
+}
