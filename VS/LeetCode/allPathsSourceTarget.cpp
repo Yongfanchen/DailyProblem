33ea0dc52414d8d03f38f6d0c955c44c5c215a99
cyf <346515575@qq.com>
2023-08-08 23:42:53 +0800

    环检测算法和拓扑排序

--- VS/LeetCode/allPathsSourceTarget.cpp
+++ VS/LeetCode/allPathsSourceTarget.cpp
@@ -8,7 +8,7 @@
 #include <queue>
 #include <unordered_set>
 using namespace std;
-class Solution {
+class Solution1 {
 private:
 	vector<vector<int>> res;
 public:
@@ -25,5 +25,31 @@ public:
 		if (s == n - 1) res.push_back(path);
 		for (int v : graph[s]) traverse(graph, v, path);
 		path.pop_back();
+	}
+};
+
+
+// ÷À∫“ª±È DFS
+
+class Solution {
+private:
+	vector<vector<int>> res;
+public:
+
+	vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+		vector<int> path;
+		dfs(graph, path, 0);
+		return res;
+	}
+	void dfs(vector<vector<int>> &graph,vector<int> &track, int s) {
+		track.push_back(s);
+		int n = graph.size();
+		if (s == n - 1) {
+			res.push_back(track);
+		}
+		for (int i : graph[s]) {
+			dfs(graph, track, i);
+		}
+		track.pop_back();
 	}
 };
